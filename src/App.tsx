4815395a461781b0b7d6b9bd2d631751821bca20
BenVermeulen <ben.vermeulen@student.ap.be>
2023-12-16 20:27:09 +0100

    hartvorm volledig gecentreerd

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import OneRowOfImagesDiv from './OneRowOfImagesDiv';
 
 const App = () => {
-
-  //2 divven met 11 kan ik korter schrijven (loop)
-
   return (
     <div>
 
-      <div style={{ display: 'flex', justifyContent: 'center' }} >
-
-        <div>
-          {Array(3).fill(null).map((item, index) => <OneRowOfImagesDiv stars={index + 4} />)}
-        </div>
-        <div>
-          {Array(3).fill(null).map((item, index) => <OneRowOfImagesDiv stars={index + 4} />)}
-        </div>
+      <div className='twoTopPartsOfHeartShape' >
+        {Array(2).fill(null).map(item =>
+          <div>
+            {Array(3).fill(null).map((item, index) => <OneRowOfImagesDiv stars={index + 4} />)}
+          </div>
+        )}
       </div>
 
-      <div>
+      <div className='bottomPartOfHeartShape'>
         {Array(7).fill(null).map((item, index) => <OneRowOfImagesDiv stars={12 - index} />)}
-      </div>
-
-      <div>
         {Array(3).fill(null).map((item, index) => <OneRowOfImagesDiv stars={4 - index} />)}
       </div>
 
