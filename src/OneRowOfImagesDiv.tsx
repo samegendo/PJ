85b3f0b06b5eff324c0b8a36a0b828c20346d8c9
BenVermeulen <ben.vermeulen@student.ap.be>
2023-12-18 21:58:01 +0100

    foto's draaien nu bij hover'

--- src/OneRowOfImagesDiv.tsx
+++ src/OneRowOfImagesDiv.tsx
@@ -4,9 +4,27 @@ import './OneRowOfImagesDiv.css';
 //         {Array(stars).fill(null).map(item => <div style={{ backgroundColor: 'yellow' }} >*</div>)}
 
 const HalfDiv = ({ stars }: any) => {
+    const [showText, setShowText] = useState<any>(false);
+    //onClick={() => setShowText((item: any) => !item)}
+
+    //   {Array(stars).fill(null).map(item => <img src="favicon.ico" alt="" />)}
+
     return (
         <div className='OneRowOfImagesDiv' >
-            {Array(stars).fill(null).map(item => <img src="favicon.ico" alt="" />)}
+            {Array(stars).fill(null).map(item =>
+                <div className="flip-box">
+                    <div className="flip-box-inner">
+                        <div className="flip-box-front">
+                            <img src="favicon.ico" alt="Paris" style={{ width: "30px", height: "30px" }} />
+                        </div>
+                        <div className="flip-box-back">
+                            <h2>Paris</h2>
+                            <p>What an amazing city</p>
+                        </div>
+                    </div>
+                </div>
+            )}
+
         </div>
     );
 }
