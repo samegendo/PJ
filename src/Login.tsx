e14290e1db1067f143367dc12f84c4e38161cb6f
BenVermeulen <ben.vermeulen@student.ap.be>
2024-02-08 20:40:04 +0100

    wachtwoord toegevoegd

---
+++ src/Login.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+const Login = ({ password, setPassword }: any) => {
+
+    return (
+        <div>
+            <label>Enter password</label>
+            <input type="text" value={password} onChange={(event) => setPassword(event.target.value)} />
+        </div>
+    )
+}
+
+
+export default Login;
