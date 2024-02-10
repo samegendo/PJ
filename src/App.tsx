30ac2bda15a3c9526ffd61cbc5e3893df7f25ce0
BenVermeulen <ben.vermeulen@student.ap.be>
2024-02-10 20:31:28 +0100

    audio toegevoegd halverwege

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Login from './Login';
 
 const App = () => {
   const [password, setPassword] = useState<any>(null);
-  const data = ["31/03/2012", "31/3/2012", "31-03-2012", "31-3-2012", "31032012", "3132012"]
+  const data = ["31/03/2012", "31/3/2012", "31-03-2012", "31-3-2012", "31032012", "3132012"];
+  const audio = new Audio("/wakeup.mp3");
 
   const handlePassword = (event: any) => {
     setPassword(event)
@@ -18,6 +19,8 @@ const App = () => {
 
   return (
     <div>
+      <button onClick={() => audio.play()}>Play Sound</button>
+      <button onClick={() => { audio.pause(); audio.currentTime = 0; }}>Stop Sound</button>
       {password && data.includes(password)
         ? <Heart />
         : <Login password={password} setPassword={handlePassword} />
